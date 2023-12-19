c639bbd698e5328211597f02a6294907162445e1
Lawrence LAU <lawrence.lau@qfpay.com>
2023-12-19 14:30:38 +0800

    upgrade

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
-FROM squidfunk/mkdocs-material:8.2.9 
-
-RUN pip install mkdocs-material
+FROM squidfunk/mkdocs-material:9.4.10
 
 RUN apk update
 RUN apk add bash inotify-tools
+
+RUN pip install mkdocs-material
+RUN pip install mkdocs-git-revision-date-localized-plugin
+
 
 CMD start.sh
---
+++ src/custom_theme/hooks/shortcodes.py
@@ -0,0 +1,63 @@
+
+from __future__ import annotations
+
+import posixpath
+import re
+
+from mkdocs.config.defaults import MkDocsConfig
+from mkdocs.structure.files import File, Files
+from mkdocs.structure.pages import Page
+from re import Match
+
+# -----------------------------------------------------------------------------
+
+# Resolve path of file relative to given page - the posixpath always includes
+# one additional level of `..` which we need to remove
+def _resolve_path(path: str, page: Page, files: Files):
+    path, anchor, *_ = f"{path}#".split("#")
+    path = _resolve(files.get_file_from_path(path), page)
+    return "#".join([path, anchor]) if anchor else path
+
+# Resolve path of file relative to given page - the posixpath always includes
+# one additional level of `..` which we need to remove
+def _resolve(file: File, page: Page):
+    path = posixpath.relpath(file.src_uri, page.file.src_uri)
+    return posixpath.sep.join(path.split(posixpath.sep)[1:])
+
+
+
+# -----------------------------------------------------------------------------
+# Hooks
+# -----------------------------------------------------------------------------
+
+# @todo
+def on_page_markdown(
+    markdown: str, *, page: Page, config: MkDocsConfig, files: Files
+):
+
+    # Replace callback
+    def replace(match: Match):
+        type, args = match.groups()
+        args = args.strip()
+        
+        if type == "version":
+            return _badge_for_version(args, page, files)
+        
+        # Otherwise, raise an error
+        raise RuntimeError(f"Unknown shortcode: {type}")
+
+    # Find and replace all external asset URLs in current page
+    return re.sub(
+        r"<!-- md:(\w+)(.*?) -->",
+        replace, markdown, flags = re.I | re.M
+    )
+
+
+# Create badge
+def _badge(icon: str, text: str = "", type: str = ""):
+    return f'<span class="badge {type}"><i class="{icon}"></i> {text}</span>'
+
+# Create badge for version
+def _badge_for_version(text: str, page: Page, files: Files):
+    # return <span> <i class="fa fa-tag"></i> 4.31.3</span>
+    return _badge("fa fa-tag", text, "version")
